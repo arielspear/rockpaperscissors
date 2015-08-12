caf656761aecedf0c15fd060a3f30942da35642d
Ariel Spear and Haley Harrison <student@epicodus.com>
2015-08-12 15:25:34 -0700

    gradle runs

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,31 +3,56 @@ import java.util.Arrays;
 import static spark.Spark.*;
 import java.util.ArrayList;
 import java.util.Random;
+import java.io.Console;
+import spark.template.velocity.VelocityTemplateEngine;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+
 
 public class App {
 
   public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "templates/home.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+    Console myConsole = System.console();
     Random randomGenerator = new Random();
-    Integer randomNum = new Random();
-    ArrayList<String> computerOptions = ["rock", "paper", "scissors"];
+    // Random randomNum = new Random();
+    ArrayList<String> computerOptions = new ArrayList<String>(); {{
+      computerOptions.add("rock");
+      computerOptions.add("paper");
+      computerOptions.add("scissors");
+    }};
+    //double brace initialization
+
+    String computerStringOptions[] = new String[computerOptions.size()];
+    String array[] = new String[computerOptions.size()];
+      for(int i =0;i<computerOptions.size();i++){
+        array[i] = computerOptions.get(i);
+      }
+
     //random number between 0 and 2
-    Integer computerRandomInt = randomGenerator.nextInt(3);
+    int computerRandom = randomGenerator.nextInt(3);
     //array of moves: move name, beaten by
-    String computerChoice = computerOptions[computerRandomInt];
+    String computerChoice = computerStringOptions[computerRandom];
     String [][] moves = {
       { "rock", "paper", },
       { "scissors", "rock", },
       { "paper", "scissors", },
     };
-
+    System.out.println(computerChoice);
 
     }
 
   public Boolean checkWinner(String input1, String input2) {
-    
-    return false;
 
+    return false;
 
     }
 }

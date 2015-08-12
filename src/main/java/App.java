251d7a45b63441ac5eb1e36161dae16e7a746be9
Ariel Spear and Haley Harrison <student@epicodus.com>
2015-08-12 12:15:23 -0700

    lunch break

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,13 +2,32 @@ import java.util.List;
 import java.util.Arrays;
 import static spark.Spark.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class App {
 
   public static void main(String[] args) {
+
+    Random randomGenerator = new Random();
+    Integer randomNum = new Random();
+    ArrayList<String> computerOptions = ["rock", "paper", "scissors"];
+    //random number between 0 and 2
+    Integer computerRandomInt = randomGenerator.nextInt(3);
+    //array of moves: move name, beaten by
+    String computerChoice = computerOptions[computerRandomInt];
+    String [][] moves = {
+      { "rock", "paper", },
+      { "scissors", "rock", },
+      { "paper", "scissors", },
+    };
+
+
     }
 
   public Boolean checkWinner(String input1, String input2) {
+    
     return false;
+
+
     }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -10,4 +10,18 @@ public class AppTest {
         assertEquals(true, testApp.checkWinner("Rock", "Scissors"));
 
     }
+    @Test
+    public void checkWinner_paperBeatsRock_true() {
+        App testApp = new App();
+        assertEquals(true, testApp.checkWinner("Paper", "Rock"));
+
+    }
+
+    @Test
+    public void checkWinner_scissorsBeatsPaper_true() {
+        App testApp = new App();
+        assertEquals(true, testApp.checkWinner("Scissors", "Paper"));
+
+    }
+
 }
